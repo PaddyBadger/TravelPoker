842b9de18bc7c954f80f9479d6926bd63c5e97f5
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-09-30 13:52:06 -0300

    Routes file to guide route creation

--- travelpoker/app/assets/javascripts/application.js
+++ travelpoker/app/assets/javascripts/application.js
@@ -1,15 +1,31 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+			$=jQuery;
+
+		$(document).ready(function(){
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_4').camera({
+			height: '500px',
+			loader: 'none',
+			pagination: false,
+			thumbnails: false,
+			navigation: false,
+			time: '10000',
+			navigationHover: false,
+			hover: false,
+			opacityOnGrid: false,
+			imagePath: '../images/',
+			fx:'simpleFade'
+				
+
+			});
+
+			$('.pokerHover,.trip,.deck').hover(function(){
+			$(this).children('.tripMeta,.pokerHover').fadeIn(500);
+			},
+			function(){
+			$('.dk_container.span3.dk_shown.dk_theme_default').removeClass('dk_open');
+			$(this).children('.tripMeta,.pokerHover').fadeOut(500);
+			});
+			});
+			});
+			

d73d9cf160829d6b465efd8002699d6911de2ba6
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-09-30 21:48:22 -0300

    added routes.rb

---
+++ tp/TravelPoker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelPoker::Application.config.secret_token = '29c86baaeffbb22e6789ba8992e2462c61b7934dbf98a7c36a46448c60b544233882a1cdad2ecb9000c0885894fb71c0c13ee333f3ad8701b8725ac21081deb0'

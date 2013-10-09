acf62f2c91b71c450f363ea6c49bbbca132c8af8
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-10-08 22:37:17 -0300

    card and deck views

--- travelpoker/app/controllers/comments_controller.rb
+++ travelpoker/app/controllers/comments_controller.rb
@@ -1,19 +1,16 @@
 class CommentsController < ApplicationController
   def create
     @card = Card.find(params[:card_id])
-    @comments = @card.comments
-
+    
     @comment = current_user.comments.build(params[:comment])
     @comment.card = @card
-    @new_comment = Comment.new
 
       if @comment.save
         flash[:notice] = "Comment was created."
       else
         flash[:error] = "There was an error saving the comment. Please try again."
       end
-        redirect_to [@card]
-    end
+      redirect_to [@card]
   end
 
   def destroy
@@ -26,7 +23,6 @@ class CommentsController < ApplicationController
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
     end
-      redirect_to [@card]
-    end
+    redirect_to [@card]
   end
 end
--- travelpoker/db/seeds.rb
+++ travelpoker/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+europe = Deck.create(title: 'Eating in Europe', location: 'Europe', image: 'http://t0.gstatic.com/images?q=tbn:ANd9GcSNVsO5DNbbKJr4VWQIiQCCz40QhvjV4LBccVGo3gzSDdOLPfIOJQ')
+
+world = Deck.create(title: 'An Incredible Gap Year', location: 'The World', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1LpqxFgUSjPhU6SENa9kxHH7au1IM4Mz5lcewo4Zyuv9X7nUb')
+
+london = Card.create(title: 'Saunter Down South Bank', location: 'London, UK', image: 'http://photorefraction.files.wordpress.com/2011/03/london-south-bank-1.jpg', url: 'n/a', description: 'If you\'re lucky with the weather, nothing in London beats a wander down South Bank in a crisp sunny day')
+
+nyc = Card.create(title: 'Kayack on the Hudson', location: 'Manhattan, New York', image: 'http://paddleforourplanet.org/wp-content/uploads/2010/09/5036264103_57198a78e9.jpg', url: 'n/a', description: 'A novel way to see New York from the river, you can kayak for free on pier 40')
+
+paris = Card.create(title: 'Put Your Lock on Pont de l\'Archevêché', location: 'Paris, France', image: 'https://mw2.google.com/mw-panoramio/photos/small/12455444.jpg', url: 'n/a', description: 'If you\'re lucky with the weather, nothing in London beats a wander down South Bank in a crisp sunny day')
+
+rome = Card.create(title: 'Get Spititual in the Sistine Chapel', location: 'Rome, Italy', image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRarVLHpL1cTT5el9tjp3BqbTZaZoWRN2M04meKwicDarw7RiLH', url: 'n/a', description: 'A novel way to see New York from the river, you can kayak for free on pier 40')
+
+rio = Card.create(title: 'Live it Up in Lapa', location: 'Rio De Janeiro, Brazil', image: 'http://graphics8.nytimes.com/images/2006/11/26/travel/next_600.jpg', url: 'n/a', description: 'If you\'re lucky with the weather, nothing in London beats a wander down South Bank in a crisp sunny day')
+
+toronto = Card.create(title: 'Kick Back at the Kensington Market', location: 'Toronto, Canada', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT1pmpb3Xt3vU1z3HHukSmn_v1zhbXK-fLEnYmPZGO21-5tRuJl', url: 'n/a', description: 'A novel way to see New York from the river, you can kayak for free on pier 40')
+
+buzios = Card.create(title: 'Beach Hop in a Buggy', location: 'Buzios, Brazil', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQphelMnbupTMdI7fPJbfG4DgZqfAg4Hpn2-L0JnsiugdtRvQNT', url: 'n/a', description: 'If you\'re lucky with the weather, nothing in London beats a wander down South Bank in a crisp sunny day')
+
+hanoi = Card.create(title: 'September\'s Spectacular in Hanoi', location: 'Hanoi, Vietnam', image: 'http://www.vietnamonline.com/userfile/news/dangnguyen/2011/12/Ava_Hanoi%20Firework.jpg', url: 'n/a', description: 'A novel way to see New York from the river, you can kayak for free on pier 40')
+
+
+europe.cards << london
+world.cards << london
+world.cards << nyc
